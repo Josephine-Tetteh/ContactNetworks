666b92a90a5ca95ecf08b50b1a92cfb1999d0990
Josephine-Tetteh <35694995+Josephine-Tetteh@users.noreply.github.com>
2020-03-30 13:42:41 +0200

    Update trygennet.R

--- trygennet.R
+++ trygennet.R
@@ -1,4 +1,4 @@
-N    <- 100
+N    <- 1000
 seed <- 123
 
 ## Steps
@@ -13,21 +13,13 @@ age[age==70] <- sample(70:80, length(age[age==70]), replace=TRUE)
 
 
 #Assign contacts: depends on both target age and POLYMOD data
-
-#agecont      <- orderBy(dget("./data/POLYMODtab1"), 1) # POLYMOD Tab.1
-Ndata <- dget("./data/POLYMODtab1")
-dat <- order(Ndata[1],decreasing = TRUE)
-Ndata$age=dat
-agecont      <- Ndata
+agecont      <- orderBy(dget("./data/POLYMODtab1"), 1) # POLYMOD Tab.1
 agecont$rk   <- rank(agecont$age)  # Rank age-group by contact
 POLYMODbreak <- c(0,4,9,14,19,29,39,49,59,69,80) 
 ageGrp <- cut(age, breaks=POLYMODbreak, include.lowest=1)
 ageGrp <- as.numeric(ageGrp) # mapping age to ageGrp
 agerk  <- sapply(ageGrp, function(x) agecont$rk[x==agecont$nmr])
-#age    <- rev(cbind(age, agerk)[, 1]) # large to small
-
-age <- rev(cbind(age,order(agerk,decreasing = TRUE))[,1])
-
+age    <- rev(orderBy(cbind(age, agerk), 2)[, 1]) # large to small
 
 #Sampling from contact distribution
 
@@ -48,11 +40,11 @@ Grp[is.na(Grp)] <- 15 # 70+ age-grp
 
 source("gennet.R")
 g <- genNet(N, age)
+plotNet(g)
+heatmap(M,Rowv = NA, Colv = NA)
+qqplot(degree(g), ncont, main="QQ Plot", ylab="Target contact distribution")
+qqplot(age, vertex_attr(g, "age"), main="QQ Plot", ylab="Target age distribution")
 
-## Outputs
-#png("nplot")
-# plotNet(g)
-#dev.off()
 # 
 # ![](./fig/ng1.png)
 # 
@@ -60,14 +52,14 @@ g <- genNet(N, age)
 # 
 # Compare age- and contact-distribution
 # ```R
- ks.test(age, vertex_attr(g, "age"))
+# ks.test(age, vertex_attr(g, "age"))
 # 
 # Two-sample Kolmogorov-Smirnov test
 # 
 # data:  age and vertex_attr(g, "age")
 # D = 0, p-value = 1
 # 
- ks.test(degree(g), ncont)
+# ks.test(degree(g), ncont)
 # 
 # Two-sample Kolmogorov-Smirnov test
 # 
@@ -75,33 +67,11 @@ g <- genNet(N, age)
 # D = 0, p-value = 1
 # 
 # put(1,2)
-qqplot(degree(g), ncont, main="QQ Plot", ylab="Target contact distribution")
-qqplot(age, vertex_attr(g, "age"), main="QQ Plot", ylab="Target age distribution")
 # ```
 # ![](./fig/qqplot.png)
 # 
 # 
 # Compare contact-matrix
 # ![](./fig/matrixCompare.png)
-
-# pdf(file="tmp1.pdf",width=5,height=5)
-qqplot(degree(g), ncont, main="QQ Plot", ylab="Target contact distribution")
-# dev.off()
-
-
-# pdf(file="plot1.pdf",width=5,height=5)
-plotNet(g)
-# dev.off()
-
-
-library(RColorBrewer)
-#redgreen <- c("cyan", "magenta","white") 
-#pal <- colorRampPalette(redgreen)(100)#pdf(file="matr.pdf",width=15,height=15)
-heatmap(M,Rowv = NA, Colv = NA)
-#dev.off()
-
-# 
-# M = data.frame(degree(g),age)
-# colnames(M)<-c('g','age')
 
 
