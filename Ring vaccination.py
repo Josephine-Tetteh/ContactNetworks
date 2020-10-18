e5b639b6d03ffe80ad9f00759d47fb407461c511
Josephine-Tetteh <35694995+Josephine-Tetteh@users.noreply.github.com>
2020-10-18 17:19:43 +0200

    Updated simulation function

--- Ring vaccination.py
+++ Ring vaccination.py
@@ -55,7 +55,7 @@ def double_smoothlog(time, bound1 , bound2, rate1 , rate2 , midpoint1 , midpoint
 
 def simfxn(Time,popul):
     bound1 = 0.028
-    bound2 = 0.01
+    bound2 = 0.001
     rate1 = 0.09
     rate2 = 0.04
     midpoint1 = 50
@@ -66,12 +66,17 @@ def simfxn(Time,popul):
     pop = g.vcount()
     g.vs["state"] = "S"
     g.vs["duration"] = 0
-    g.vs["tcount"] = 0
+    g.vs["somecol"] = 'THB'
     #randomly select % of the population to be vaccinated
-    perc_vac = int(0.1*pop)
-    vac_eff = 0.4  # vaccine efficacy
+#     perc_vac = int(0*pop)
+    vac_eff = 0  # vaccine efficacy
+#     init_vac_grp = random.sample(list(range(pop)), perc_vac)
+#     g.vs[init_vac_grp]["state"] = "V"
+    #randomly select an infected node to start epidemic
     i = rd.randint(0, pop-1)
     g.vs[i]["state"] = "E"
+#     if g.vs[i]["state"] != 'V':   # if initial node is not vaccinated, then set it as exposed
+#         g.vs[i]["state"] = "E"
     
     nb_S = [pop]
     nb_E = [1]
@@ -81,45 +86,75 @@ def simfxn(Time,popul):
     exposed_vac = []
     Time = Time 
     count = 0 
-    
+    inf_nodes = []
+    g.vs["tcount"] = 0 
     for time in range(Time): #no. of days  
-        if len(g.vs.select(state_eq = "I"))> int(300):
-            cutime = time
-            inf_nodes = []
-            for inf in g.vs.select(state_eq = "I"):
-                inf_nodes.append(g.neighbors(inf))
-                for ninf in g.neighbors(inf):
-                    g.vs[ninf]['tcount'] = cutime
-                    g.vs[ninf]['state'] = 'TB'
-           
-        for qq in g.vs.select(state_eq='TB'):
-            if time == g.vs[qq.index]["tcount"] + 3:
-                g.vs[qq.index]["state"] = "V"
-
-        for n in g.vs.select(state_eq = "E"): #iterates through each node in the network
-            g.vs[n.index]["duration"] += 1 
-            if g.vs[n.index]["duration"] in range(10,21):  #(7,21)
-                g.vs[n.index]["state"] = 'I'
-                count = count + 1
-            else:
+        cutime = time
+        if len(g.vs.select(state_eq = "E"))< int(0.1*popul):
+            for n in g.vs.select(state_eq = "E"): #iterates through each node in the network
+                g.vs[n.index]["duration"] += 1 
+                if g.vs[n.index]["duration"] in range(7,21):  #(7,21)
+                    g.vs[n.index]["state"] = 'I'
                 for nb in g.neighbors(n): #iterates through neighbours of that node
-                    if g.vs[nb]["state"] == "S": 
-                        s = rd.random() #random state
-                        if s <= beta[time]:
+                    if g.vs[nb]["state"] == "S": #if node is infected...
+                        s=np.random.binomial(1, beta[time],1)
+                        if s == 1:
                             g.vs[nb]["state"] = "E" 
-                           
-        for m in g.vs.select(state_eq = "I"): #iterates through each node in the network
-            g.vs[m.index]["duration"] += 1 #from day 0 this node continues to infect                                
-            if g.vs[m.index]["duration"] in range(21,Time):
-                g.vs[m.index]["state"] = 'R'
-                
+            for m in g.vs.select(state_eq = "I"): #iterates through each node in the network
+                g.vs[m.index]["duration"] += 1 #from day 0 to infect_len this node continues to infect                                
+                for nbm in g.neighbors(m): #iterates through neighbours of that node
+                    if g.vs[nbm]["state"] == "S": #if node is infected...
+                        j=np.random.binomial(1, beta[time],1)
+                        if j == 1:
+                            g.vs[nbm]["state"] = "E"
+                if g.vs[m.index]["duration"] in range(21,Time):
+                    g.vs[m.index]["state"] = 'R'
+
+        else:
+            for inf in g.vs.select(state_eq = "E"):
+                for na in g.neighbors(inf):
+                    g.vs[na]["somecol"] = 'myo'   
+                    inf_nodes.append(na)
+
+            for ele in g.vs.select(somecol_eq = 'myo'):
+                 g.vs[ele.index]["tcount"] += 1
+
+            for then in inf_nodes:
+                if g.vs[then]["tcount"]>= 3 and g.vs[then]['state'] == 'S':
+                    B=np.random.binomial(1, (1-vac_eff)*beta[time],1)
+                    if B == 1:
+                        g.vs[then]["state"] = "V"
+
+            for n in g.vs.select(state_eq = "E"): #iterates through each node in the network
+                g.vs[n.index]["duration"] += 1 
+                if g.vs[n.index]["duration"] in range(7,21):  #(7,21)
+                    g.vs[n.index]["state"] = "I"
+                    count = count + 1
+    #             else:
+                for nb in g.neighbors(n): #iterates through neighbours of that node
+                    if g.vs[nb]["state"] == "S": #if node is infected...
+    #                     s = rd.random() #random state
+                        s=np.random.binomial(1, beta[time],1)
+                        if s == 1:
+                            g.vs[nb]["state"] = "E" 
+                for m in g.vs.select(state_eq = "I"): #iterates through each node in the network
+                    g.vs[m.index]["duration"] += 1 #from day 0 to infect_len this node continues to infect                                
+                    for nbm in g.neighbors(m): #iterates through neighbours of that node
+                        if g.vs[nbm]["state"] == "S": #if node is infected...
+        #                     j = rd.random() #random state
+                            j=np.random.binomial(1, beta[time],1)
+                            if j == 1:
+                                g.vs[nbm]["state"] = "E"
+                    if g.vs[m.index]["duration"] in range(21,Time):
+                        g.vs[m.index]["state"] = 'R'
+
         nb_S.append(len(g.vs.select(state_eq = "S"))) #no. of susceptibles in population
         nb_E.append(len(g.vs.select(state_eq = "E"))) #no. of recovereds in population
         nb_I.append(len(g.vs.select(state_eq = "I"))) #no. of infecteds in population
         nb_R.append(len(g.vs.select(state_eq = "R"))) #no. of recovereds in population
         nb_V.append(len(g.vs.select(state_eq = "V"))) #no. of recovereds in population
 
-    return(nb_S,nb_E,nb_I,nb_R,nb_V,count)
+    return(nb_S,nb_E,nb_I,nb_R,nb_V,cutime,g.vs['state'])
 
 
 # In[ ]:
